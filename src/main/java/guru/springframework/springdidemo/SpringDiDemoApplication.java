59d32e7d208d9f6177ae62b9729289671145b86e
Djonathan Barros <djonathan.barros@gmail.com>
2018-07-14 17:22:56 -0300

    created MyController and used it to test Spring Context

--- src/main/java/guru/springframework/springdidemo/SpringDiDemoApplication.java
+++ src/main/java/guru/springframework/springdidemo/SpringDiDemoApplication.java
@@ -1,12 +1,16 @@
 package guru.springframework.springdidemo;
 
+import guru.springframework.springdidemo.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDiDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringDiDemoApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(SpringDiDemoApplication.class, args);
+        MyController myController = ctx.getBean("myController", MyController.class);
+        myController.hello();
     }
 }
